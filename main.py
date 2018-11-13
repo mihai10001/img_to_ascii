288d43eb2f14ad3159d37aaf8974ac75608f6e74
Iliuta Mihai <mortvmis@gmail.com>
2018-11-13 13:19:10 +0200

    improved mapping

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import numpy as np
 np.set_printoptions(threshold=np.inf)
 
 
-def load_image_greyscale(filename):
+def import_greyscale(filename):
     img = Image.open(filename).convert('L')
-    data = np.asarray(img, dtype=np.float)
+    data = np.asarray(img, dtype=np.int32)
     return data, data.shape
 
 
@@ -15,65 +15,61 @@ def get_gcd(width, height):
 
 
 def get_median(arr):
-    return int(np.median(arr))
+    return (np.median(arr)).astype('int32')
 
 
-def return_traversed_image(divisor, width, height):
-    arr = np.zeros((height, width), dtype=np.float)
+def traverse_image(divisor, width, height):
+    patch = 2  # for viewing with a text editor, keeping aprox. the same ratio as the image
+    arr = np.zeros((height, width), dtype=np.int32)
     new_width = int(width/divisor)
-    new_height = int(height/(divisor*2))
+    new_height = int(height/(divisor*patch))
 
     start_y = height
     for row in reversed(range(0, new_height)):
         start_x = 0
-        start_y -= 2*divisor
+        start_y -= patch*divisor
         for col in range(0, new_width):
-            arr[row, col] = get_median(pix[start_y: start_y + 2*divisor, start_x: start_x + divisor])
+            arr[row, col] = get_median(pix[start_y: start_y + patch*divisor, start_x: start_x + divisor])
             start_x += divisor
 
     return arr, new_width, new_height
 
 
-def write_text(arr, mylist, width, height, result='result.txt'):
+def write_text(arr, mylist, width, height, result='result3.txt'):
     with open(result, 'w') as text:
         for row in range(0, height):
             whole_row = ''
             for col in range(0, width):
                 check = arr[row, col]
                 for element in mylist:
-                    if check >= element['start'] and check <= element['end']:
-                        whole_row += element['char']
+                    if element['st'] <= check <= element['en']:
+                        whole_row += element['ch']
             text.write(whole_row + '\n')
 
 
 def get_style():
-    # style = " .:-=+*#%@"
-    style = " .`'\"^,:;Il!i><~+jrdbk*#M@"
-    mil = []
-    start = 0
-    part = int(255/len(style))
+    return " .`'\"^,:;Il!i><~+jrdbk*#M@"
 
-    for c in reversed(style):
-        mydict = {}
-        mydict['char'] = c
-        mydict['start'] = start
-        mydict['end'] = start + part
-        mil.append(mydict)
-        start += part + 1
 
-    mil[-1]['end'] = 255
-    return mil
+def get_mapping():
+    style = get_style()
+    mapping, start, part = [], 0, int(255/len(style))
+    print(mapping, start, part)
+    mapping = [{'ch': x, 'st': start, 'en': start+part} for start, x in enumerate(style[::-1], start+part)]
+    #mapping[-1][2] = 255
+    print(mapping)
+    return mapping
 
 
 if __name__ == "__main__":
-    pix, (h, w) = load_image_greyscale('test1.jpg')
-    hood = 4
-    arr, new_w, new_h = return_traversed_image(hood, w, h)
-    style = get_style()
-    write_text(arr, style, new_w, new_h)
+    pix, (h, w) = import_greyscale('test1.jpg')
+    hood = 4  # get_gcd(w, h)
+    arr, new_w, new_h = traverse_image(hood, w, h)
+    mapp = get_mapping()
+    #write_text(arr, mapp, new_w, new_h)
 
 # STEPS:
 # IMPORT IMAGE(CONVERTED TO GREYSCALE)
 # SELECT NEIGHBOURHOOD (4 = 4X4 = 16 PIXELS) get_gcd(w, h)
-# SELECT STYLE
+# SELECT MAPPING BASED OF STYLE
 # WRITE TEXT
